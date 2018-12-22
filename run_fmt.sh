6e1ba39d357b44aaa4e8d8505ecdcc9b4b5a577f
Hansung Kim <stephen422@gmail.com>
2018-12-23 00:54:45 +0900

    run_fmt.sh: parallelize using background jobs, output to file

--- run_fmt.sh
+++ run_fmt.sh
@@ -7,18 +7,18 @@ fi
 
 echo "args:" $@
 echo "All perfect:"
-./sim-outorder -cache:dl1perfect -bpred perfect -cache:il1perfect -cache:il2perfect -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ 2>&1 | grep CPI
+./sim-outorder -cache:dl1perfect -bpred perfect -cache:il1perfect -cache:il2perfect -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ >output.log.perf 2>&1 &
 echo "+ real dl1c:"
-./sim-outorder                   -bpred perfect -cache:il1perfect -cache:il2perfect -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ 2>&1 | grep CPI
+./sim-outorder                   -bpred perfect -cache:il1perfect -cache:il2perfect -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ >output.log.dl1c 2>&1 &
 echo "+ real bpred:"
-./sim-outorder                                  -cache:il1perfect -cache:il2perfect -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ 2>&1 | grep CPI
+./sim-outorder                                  -cache:il1perfect -cache:il2perfect -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ >output.log.bpred 2>&1 &
 echo "+ real il1c:"
-./sim-outorder                                                    -cache:il2perfect -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ 2>&1 | grep CPI
+./sim-outorder                                                    -cache:il2perfect -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ >output.log.il1c 2>&1 &
 echo "+ real il2c:"
-./sim-outorder                                                                      -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ 2>&1 | grep CPI
+./sim-outorder                                                                      -tlb:itlbperfect -cache:dl2perfect -tlb:dtlbperfect $@ >output.log.il2c 2>&1 &
 echo "+ real itlb:"
-./sim-outorder                                                                                       -cache:dl2perfect -tlb:dtlbperfect $@ 2>&1 | grep CPI
+./sim-outorder                                                                                       -cache:dl2perfect -tlb:dtlbperfect $@ >output.log.itlb 2>&1 &
 echo "+ real dl2c:"
-./sim-outorder                                                                                                         -tlb:dtlbperfect $@ 2>&1 | grep CPI
+./sim-outorder                                                                                                         -tlb:dtlbperfect $@ >output.log.dl2c 2>&1 &
 echo "+ real dtlb:"
-./sim-outorder                                                                                                                          $@ 2>&1 | grep CPI
+./sim-outorder                                                                                                                          $@ >output.log.dtlb 2>&1 &
